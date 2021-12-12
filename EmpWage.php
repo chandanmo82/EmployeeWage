cda24f9e3beb9a5460b36ad9254f26ee3ba6a13e
Your Name <you@example.com>
2021-12-12 20:21:46 +0530

    Added : parttime employee

--- EmpWage.php
+++ EmpWage.php
@@ -1,16 +1,19 @@
 <?php
 echo "Welcome to Employee Wage Problem <br>";
-$check = rand(0,1);
-$IS_PRESENT = 1;
+$check = rand(0,2);
+$IS_PARTTIME = 1;
+$IS_FULLTIME = 2;
 $EMP_RATE_PER_HOUR = 20;
 $empHrs = 0;
 $empWage = 0;
 
-
-if ($check == $IS_PRESENT){
+if ($check == $IS_PARTTIME){
+    $empHrs = 4;
+}
+elseif ($check == $IS_FULLTIME) {
     $empHrs = 8;
 }
-else{
+else {
     $empHrs = 0;
 }
 $empWage = $EMP_RATE_PER_HOUR *  $empHrs;

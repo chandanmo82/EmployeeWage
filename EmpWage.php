ee6ecb5c757870d2d4e6e9f20fde230233e4bdac
Your Name <you@example.com>
2021-12-12 20:58:13 +0530

    Refactored : Using switch case

--- EmpWage.php
+++ EmpWage.php
@@ -6,15 +6,15 @@ $IS_FULLTIME = 2;
 $EMP_RATE_PER_HOUR = 20;
 $empHrs = 0;
 $empWage = 0;
-
-if ($check == $IS_PARTTIME){
-    $empHrs = 4;
-}
-elseif ($check == $IS_FULLTIME) {
-    $empHrs = 8;
-}
-else {
-    $empHrs = 0;
+switch ($check){
+    case $IS_PARTTIME :
+        $empHrs = 4;
+        break;
+    case $IS_FULLTIME :
+        $empHrs = 8;
+        break;
+    default :
+        $empHrs = 0;         
 }
 $empWage = $EMP_RATE_PER_HOUR *  $empHrs;
 echo "Employee Wage :". $empWage;  

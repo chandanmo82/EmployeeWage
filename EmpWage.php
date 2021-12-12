a82e961914da8c6c836a085e6d073dd996a12568
Your Name <you@example.com>
2021-12-12 21:51:55 +0530

    Refactored : calculating wages for a month

--- EmpWage.php
+++ EmpWage.php
@@ -1,21 +1,28 @@
 <?php
 echo "Welcome to Employee Wage Problem <br>";
-$check = rand(0,2);
 $IS_PARTTIME = 1;
 $IS_FULLTIME = 2;
 $EMP_RATE_PER_HOUR = 20;
+$NUM_OF_WORKING_DAYS = 20;
 $empHrs = 0;
 $empWage = 0;
-switch ($check){
-    case $IS_PARTTIME :
-        $empHrs = 4;
-        break;
-    case $IS_FULLTIME :
-        $empHrs = 8;
-        break;
-    default :
-        $empHrs = 0;         
+$totalEmpwage = 0;
+for ($day = 0;$day < $NUM_OF_WORKING_DAYS;$day++){
+    $check = rand(0,2);
+    switch ($check){
+        case $IS_PARTTIME :
+            $empHrs = 4;
+            break;
+        case $IS_FULLTIME :
+            $empHrs = 8;
+            break;
+        default :
+            $empHrs = 0;         
+    }
+    $empWage = $EMP_RATE_PER_HOUR *  $empHrs;
+    $totalEmpwage += $empWage;
+    echo "Employee Wage :". $empWage;
+    echo "<br>";
 }
-$empWage = $EMP_RATE_PER_HOUR *  $empHrs;
-echo "Employee Wage :". $empWage;  
+echo "Total Employee Wage :".$totalEmpwage;
 ?>

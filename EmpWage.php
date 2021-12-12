f59ad28058fb32149157380970ae01e6f84b5462
Your Name <you@example.com>
2021-12-12 18:48:00 +0530

    refactored : employee is present or absent

--- EmpWage.php
+++ EmpWage.php
@@ -1,3 +1,11 @@
 <?php
 echo "Welcome to Employee Wage Problem <br>";
+$check = rand(0,1);
+$IS_PRESENT = 1;
+if ($check == $IS_PRESENT){
+    echo "Employee is present";
+}
+else{
+    echo "Employee is absent";
+}
 ?>
